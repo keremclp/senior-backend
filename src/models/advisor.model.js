04cbf426008c24e57f5a500e1f9cc7a029470e63
keremclp <kerem00962@gmail.com>
2025-04-18 10:58:18 +0300

    feat: update project structure by adding advisor and user models, and upload routes; modify package.json and package-lock.json for new dependencies

---
+++ src/models/advisor.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const AdvisorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide name"],
+        minlength: 3,
+        maxlength: 50,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, "Please provide email"],
+        validate: {
+        validator: (value) => {
+            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+        },
+        message: "Please provide valid email",
+        },
+    },
+    info: { 
+        type: String,
+        required: [true, "Please provide info"],
+    },
+    secondInfo: { 
+        type: String,
+        required: [true, "Please provide second info"],
+    },
+    tags: {
+        type: [String],
+        required: [true, "Please provide tags"],
+    },
+    prefix: {
+        type: String,
+        required: [true, "Please provide prefix"],
+    },
+});
+
+const Advisor = mongoose.model("Advisor", AdvisorSchema);
+
